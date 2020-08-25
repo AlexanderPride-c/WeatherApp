41e953e96e848400ea54da9571bb40ab5e8f826b
a.pride <a.pride.msk@gmail.com>
2020-08-25 19:55:40 +0300

    app design, work with storyboard

---
+++ WeatherApp/Models/NetworkingWeatherManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkingWeatherManager.swift
+//  WeatherApp
+//
+//  Created by Александр Прайд on 25.08.2020.
+//  Copyright © 2020 Alexander Pride. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkingWeatherManager {
+    
+}
--- WeatherApp/ViewControllers/ViewController.swift
+++ WeatherApp/ViewControllers/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLIkeTemperatureLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
